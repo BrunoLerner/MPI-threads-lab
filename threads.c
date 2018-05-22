abd7fe01c54b988d1e111474abcaccff9093a7a7
BrunoLerner <bru.lerner@gmail.com>
2018-05-22 09:44:26 -0300

    Generating random matrix

--- threads.c
+++ threads.c
@@ -6,41 +6,49 @@
 int main(){
 
     // Alocando espaço pra matriz
-    int **matrixMalloc, i, j, matrix[size][size];
-    matrixMalloc = malloc(size * sizeof(int *));
+    int **matrix, i, j;
+    matrix = malloc(size * sizeof(int *));
     
-    if(matrixMalloc == NULL) {
+    if(matrix == NULL) {
         printf("Out of memory\n");
         exit(1);
     }
 
-    for(int i = 0; i < size; i++) {
-        matrixMalloc[i] = malloc(size * sizeof(int));
-        if(matrixMalloc[i] == NULL) {
+    for(i = 0; i < size; i++) {
+        matrix[i] = malloc(size * sizeof(int));
+        if(matrix[i] == NULL) {
             printf("Out of memory\n");
             exit(1);
         }
     }
-    
-    FILE *matrixFile = fopen("matrix.txt","r");
-    if(matrixFile == NULL) {
-        printf("file could not be opened");
-        exit(1);
-    }
-
-    i=0;
-    while(!feof(matrixFile)) {
-        char temp[size], *token;
-        fscanf(matrixFile, "%s", temp);
-        token = strtok(temp, ",");
-        j=0;
-        while(token != NULL) {
-            matrix[i][j] = atoi(token);
-            token = strtok(NULL, ",");
-            j++;
+    // Gerando a matriz aleatoriamente
+    for(i = 0; i < size; i++) {
+        for(j = 0; j < size; j++) {
+            matrix[i][j] = rand() % 2;
         }
-        i++;
     }
+    
+    // Lendo a matriz de um arquivo
+
+    // FILE *matrixFile = fopen("matrix.txt","r");
+    // if(matrixFile == NULL) {
+    //     printf("file could not be opened");
+    //     exit(1);
+    // }
+
+    // i=0;
+    // while(!feof(matrixFile)) {
+    //     char temp[size], *token;
+    //     fscanf(matrixFile, "%s", temp);
+    //     token = strtok(temp, ",");
+    //     j=0;
+    //     while(token != NULL) {
+    //         matrix[i][j] = atoi(token);
+    //         token = strtok(NULL, ",");
+    //         j++;
+    //     }
+    //     i++;
+    // }
 
 
     return 0;
