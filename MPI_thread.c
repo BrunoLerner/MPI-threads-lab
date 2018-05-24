038c5ae46f9eb9de5dcdf8af0ab08aea736844a7
BrunoLerner <bru.lerner@gmail.com>
2018-05-24 20:59:15 -0300

    almost there

--- MPI_thread.c
+++ MPI_thread.c
@@ -4,6 +4,40 @@
 #include <time.h>
 #include "mpi.h"
 #define size 16000
+#define nThread 2
+
+struct thread_args {
+    int index;
+    int procRank;
+    int **matrix;
+    int *diagonal;
+};
+
+int sum[2];
+
+void * partialSum (void *matrixAndIndex) {
+    struct thread_args * args = (struct thread_args *) matrixAndIndex;
+    int index = args->index;
+    int procRank = args->procRank;
+    int **matrix = args->matrix;
+    int *diagonal = args->diagonal;
+    int columns = size/2, rows = size/2, rowStart = 0, columnStart = 0;
+    printf("Esse é o thread %d do processador %d calculando uma metade da matriz.\n", index, procRank);
+
+    if (procRank == 0 && index == 1) columnStart = size/2;
+    else if (procRank == 1 && index == 0) rowStart = size/2;
+    else if (procRank == 1 && index == 1) {
+        rowStart = size/2;
+        columnStart = size/2;
+    }
+    
+    for(int i = rowStart; i < rowStart + rows; i++) {
+        for(int j = columnStart; j < columnStart + columns; j++) {
+            matrix[i][j] *= diagonal[i];
+            sum[index] += matrix[i][j];
+        }
+    }
+}
 
 int ** getMatrix() {
     // Alocando espaço pra matriz
@@ -58,27 +92,57 @@ int main(int argc, char** argv){
         int **matrix = getMatrix();
         int *matrixArray = matrixToArray(&matrix);
         MPI_Send(&matrixArray, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        int columns = size, rows = size/2, start = 0;
         printf("Esse é o nó %d calculando metade da matriz", myRank);
-        for(int i = start; i < rows; i++) {
-            for(int j = 0; j < columns; j++) {
-                matrix[i][j] *= matrix[i][i];
-                localSum += matrix[i][j];
-            }
-        }      
+
+        struct thread_args* matrixAndIndex;
+        int *diagonal = malloc(size * sizeof(int));
+
+        for(int i = 0; i < size ; i++) {
+            diagonal[i] = matrix[i][i];
+        }
+
+        for(int i = 0; i < nThreads; i++) {
+            matrixAndIndex = malloc(sizeof(struct thread_args));
+            matrixAndIndex->index = i;
+            matrixAndIndex->procRank = myRank;
+            matrixAndIndex->matrix = matrix;
+            matrixAndIndex->diagonal = diagonal;
+            pthread_create(&threads[i], NULL, partialSum, (void *) matrixAndIndex);
+        }
+        
+        for(int i = 0; i < nThreads; i++) {
+            pthread_join(threads[i], NULL);
+        }
+
+        localSum = sum[0] + sum[1];
     }
     else if(myRank == 1) {
         matrixArray = (int*) malloc((SIZE/2)*sizeof(int));
         MPI_Recv(&matrixArray, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         int **matrix = arrayToMatrix(&matrixArray);
-        int columns = size, rows = size/2, start = size/2;
+        printf("Esse é o nó %d calculando metade da matriz", myRank);
 
-        for(int i = start; i < rows; i++) {
-            for(int j = 0; j < columns; j++) {
-                matrix[i][j] *= matrix[i][i];
-                localSum += matrix[i][j];
-            }
+        struct thread_args* matrixAndIndex;
+        int *diagonal = malloc(size * sizeof(int));
+
+        for(int i = 0; i < size ; i++) {
+            diagonal[i] = matrix[i][i];
         }
+
+        for(int i = 0; i < nThreads; i++) {
+            matrixAndIndex = malloc(sizeof(struct thread_args));
+            matrixAndIndex->index = i;
+            matrixAndIndex->procRank = myRank;
+            matrixAndIndex->matrix = matrix;
+            matrixAndIndex->diagonal = diagonal;
+            pthread_create(&threads[i], NULL, partialSum, (void *) matrixAndIndex);
+        }
+        
+        for(int i = 0; i < nThreads; i++) {
+            pthread_join(threads[i], NULL);
+        }
+
+        localSum = sum[0] + sum[1];
     }
 
     MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0,
