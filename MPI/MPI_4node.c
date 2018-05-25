ae0ef8ef22bdb5bd5de43dd3204ef9654084d93b
tergol1 <36600936+tergol1@users.noreply.github.com>
2018-05-24 23:31:55 -0300

    Update MPI_4node.c

--- MPI/MPI_4node.c
+++ MPI/MPI_4node.c
@@ -71,6 +71,9 @@ int ** arrayToMatrix(int **matrixArray) {
 }
 
 int main(int argc, char** argv){
+    
+    struct timeval startTime, endTime;
+    
     MPI_Init(&argc, &argv);
 
     int localSum = 0, globalSum, myRank;
@@ -83,6 +86,8 @@ int main(int argc, char** argv){
         int columns = size/2, rows = size/2, rowStart = 0, columnStart = 0;
         printf("Esse é o nó %d calculando um quarto da matriz e a distribuindo", myRank);
 
+        gettimeofday(&startTime, NULL);
+        
         MPI_Send(&matrixArray, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
         
         for (int i = rowStart; i < rowStart + rows; i++) {
@@ -152,7 +157,11 @@ int main(int argc, char** argv){
 
     // Print the result
     if (myRank == 0) {
-        printf("A soma dos elementos da matriz resultante é = %d\n", global_sum));
+        
+        gettimeofday(&endTime, NULL);
+        double time = (endTime.tv_sec*1000000 + endTime.tv_usec) - (startTime.tv_sec*1000000 +  startTime.tv_usec);
+        
+        printf("A soma dos elementos da matriz resultante é = %d\n, tempo é = %d\n", global_sum, time/1000000.0));
     }
 
     
